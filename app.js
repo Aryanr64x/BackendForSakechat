98a681a0976aa20bfacda14865582c356b4bffd9
saket <aryansaket@yahoo.com>
2023-03-02 21:15:28 +0530

    SIgn SIgnup completion

---
+++ app.js
@@ -0,0 +1,23 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+import { errorHandler } from './controllers/errorController.js';
+import authRouter from './routers/authRouter.js';
+
+const app = express();
+app.use(express.json())
+app.use(cors())
+
+
+app.use('/api', authRouter);
+
+app.use(
+    errorHandler
+);
+
+
+mongoose.connect('mongodb+srv://saket:annesha@cluster0.d5czosp.mongodb.net/?retryWrites=true&w=majority')
+
+app.listen(8000, ()=>{
+    console.log("The server is now running at port 8000")
+});
